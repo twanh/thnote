c2af02749b54161c69fe60a36dcd60b69dc2e561
Twan H <Twan H>
2016-10-20 15:49:47 +0200

    fixed static

--- tnote/settings.py
+++ tnote/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
